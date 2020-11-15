663cccba0edc775dfaaf24a8ae0b6c5f5afd754a
Muhammad-Botir <xasanovich01@icloud.com>
2020-11-15 19:19:26 +0500

    Mortgage Calculator

--- Numbers/Mortgage_calculator.py
+++ Numbers/Mortgage_calculator.py
@@ -3,8 +3,7 @@
 # Calculate the monthly payments of a fixed term mortgage
 # over given Nth terms at a given interest rate.
 # Figure out how long it will take the user to pay back the loan
-# For added complexity, add an option for users to select the compounding interval
-# Monthly, Weekly, Daily, Continually
+
 
 class Mortgage:
     def __init__(self):
